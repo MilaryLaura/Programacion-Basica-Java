ae2b62acbdb98334b81bfc1aa763b7fa504c1fe6
MilaryLaura <Jhon_11>
2024-04-23 13:20:21 -0500

    first commit

---
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,8 @@
+package holamundo;
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Milary Yulyana Laura Machaca");
+        System.out.println("Hola Mundo");    
+    }
+    
+}
