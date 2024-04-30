60975a42119e17d2a23fce6c49c2a25dc5229f82
MilaryLaura <Jhon_11>
2024-04-30 13:15:44 -0500

    Clase dia Lunes 30

--- EstructuraDatos/src/estructuradatos/EstructuraDatos.java
+++ EstructuraDatos/src/estructuradatos/EstructuraDatos.java
@@ -2,23 +2,28 @@ package estructuradatos;
 
 import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
- 
+
 public class EstructuraDatos {
     
     public static void main(String[] args) {
-       Scanner scanner = new Scanner(System.in);
-       boolean salir = false;
+        
+        try {
+            Scanner scanner = new Scanner(System.in);
+           boolean salir = false;
        
-       while(!salir){
-           System.out.println("Estructura de Datos");
-           System.out.println("1.- Array");
-           System.out.println("2.- ArrayList ");
-           System.out.println("3.- HasMap");
-           System.out.println("4.- Salir");
+           while(!salir){
+               System.out.println("Estructura de Datos");
+               System.out.println("1.- Array");
+               System.out.println("2.- ArrayList ");
+               System.out.println("3.- HashMap");
+               System.out.println("4.- Salir");
            
            System.out.print("Ingrese una opcion: ");
            int opcion = scanner.nextInt();
+           
            
            switch (opcion){
                 case 1:
@@ -72,8 +77,58 @@ public class EstructuraDatos {
                    //Comprobar si un elemetnp existe
                     System.out.println("Existe: " + ListaNombres.contains("David"));
                     break;
+                
+                case 3:
+                    System.out.println(" ---------------- ");
+                    System.out.println("Bienvenido a HashMap");
+                    
+                    Map<String,Integer> edades1 = new HashMap<>();
+                    
+                    //Insertando Data
+                    edades1.put("Milary", 17);
+                    edades1.put("Marco", 22);
+                    edades1.put("Javier", 19);
+                    edades1.put("Pedro", 27);
+                    
+                    //Eliminar un Dato
+                    edades1.remove("Pedro");
+                    
+                    //Recuperar un Valor
+                    System.out.println("Edad Milary: " + edades1.get("Milary"));
+                    
+                    //Longitud del Mapa
+                    edades1.size();
+                    
+                    //Imprimir las Llaves (Key)
+                    for(String key : edades1.keySet()){
+                        System.out.println("Llaves: " + key);
+                    }
+                    
+                    //Imprimir los valores (Value)
+                    for(Integer value : edades1.values()){
+                        System.out.println("Valor: " + value);
+                    }
+                    
+                    //Imprimir las llaves y los valores
+                    for (Map.Entry<String, Integer> lv : edades1.entrySet()) {
+                        System.out.println("LLave: " + lv.getKey() + ", Valor " + lv.getValue());                     
+                    }
+                    break;
+                default:
+                    System.out.println("Elija una opcion valida");
+                    break;
            }
-       }
-       scanner.close();
+           }
+           scanner.close();
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error " + e.getMessage());
+        } finally {
+            System.out.println("GRACIAS POR USAR EL PROGRAMA");
+        }
     }
 }
+
+
+                
+       
+  
--- OperacionesComparacion/src/operacionescomparacion/OperacionesComparacion.java
+++ OperacionesComparacion/src/operacionescomparacion/OperacionesComparacion.java
@@ -32,6 +32,6 @@ public class OperacionesComparacion {
         }
     }
 }
-
+//Milary Yulyana LAURA
 
 
